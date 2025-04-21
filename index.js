58956fa1d5754693efa42b2f918b6aecf635f2d3
Julian Waller <git@julusian.co.uk>
2025-04-21 17:40:37 +0100

    wip: claude tried

--- index.js
+++ index.js
@@ -1,70 +1,75 @@
-const tcp = require('../../tcp')
-const instance_skel = require('../../instance_skel')
+// ES modules
+import { InstanceBase, TCPHelper, Regex, combineRgb } from '@companion-module/base'
 
-class instance extends instance_skel {
+class VizrtTcpEngineTrioInstance extends InstanceBase {
 	/**
 	 * Create an instance of the module
 	 *
-	 * @param {EventEmitter} system - the brains of the operation
-	 * @param {string} id - the instance ID
-	 * @param {Object} config - saved user configuration parameters
+	 * @param {Object} internal - Companion internal data
 	 * @since 1.0.0
 	 */
-	constructor(system, id, config) {
-		super(system, id, config)
-		this.actions() // export actions
-		//this.init_presets() // export presets
+	constructor(internal) {
+		super(internal)
+		this.updateActions() // export actions
 	}
 
-	updateConfig(config) {
-		//this.init_presets()
-
-		if (this.socket !== undefined) {
-			this.socket.destroy()
-			delete this.socket
-		}
-
+	/**
+	 * Process configuration updates
+	 */
+	async configUpdated(config) {
 		this.config = config
-		this.init_tcp()
+
+		// Close existing connection and create a new one
+		this.initTCP()
 	}
 
-	init() {
-		//this.init_presets()
-		this.init_tcp()
+	/**
+	 * Initialize module
+	 */
+	async init(config) {
+		this.config = config
+		this.initTCP()
 	}
 
-	init_tcp() {
+	/**
+	 * Initialize TCP connection
+	 */
+	initTCP() {
 		if (this.socket !== undefined) {
 			this.socket.destroy()
 			delete this.socket
 		}
 
-		this.status(this.STATE_WARNING, 'Connecting')
+		this.updateStatus('warning', 'Connecting')
 
 		if (this.config.host) {
-			this.socket = new tcp(this.config.host, this.config.port)
+			this.socket = new TCPHelper(this.config.host, this.config.port)
 
 			this.socket.on('status_change', (status, message) => {
-				this.status(status, message)
+				this.updateStatus(status, message)
 			})
 
 			this.socket.on('error', (err) => {
-				this.debug('Network error', err)
-				this.status(this.STATE_ERROR, err)
+				this.log('debug', 'Network error: ' + err.message)
+				this.updateStatus('error', err.message)
 				this.log('error', 'Network error: ' + err.message)
 			})
 
 			this.socket.on('connect', () => {
-				this.status(this.STATE_OK)
-				this.debug('Connected')
+				this.updateStatus('ok')
+				this.log('debug', 'Connected')
 			})
 
-			this.socket.on('data', (data) => {})
+			this.socket.on('data', (data) => {
+				// Process any received data if needed
+			})
 		}
 	}
 
-	// Return config fields for web config
-	config_fields() {
+	/**
+	 * Return config fields for web config
+	 */
+	getConfigFields() {
 		return [
 			{
 				type: 'textinput',
@@ -72,7 +77,6 @@ class instance extends instance_skel {
 				label: 'Target Machine',
 				width: 6,
 				default: '127.0.0.1',
-				//regex: this.REGEX_IP,
 			},
 			{
 				type: 'textinput',
@@ -80,7 +84,7 @@ class instance extends instance_skel {
 				label: 'Target Port',
 				width: 2,
 				default: 6100,
-				regex: this.REGEX_PORT,
+				regex: Regex.PORT,
 			},
 			{
 				type: 'dropdown',
@@ -95,24 +99,33 @@ class instance extends instance_skel {
 		]
 	}
 
-	// When module gets deleted
-	destroy() {
+	/**
+	 * Clean up when module is deleted
+	 */
+	async destroy() {
 		if (this.socket !== undefined) {
 			this.socket.destroy()
 		}
 
-		this.debug('destroy', this.id)
+		this.log('debug', 'Module destroyed: ' + this.id)
 	}
 
-	init_presets() {
+	/**
+	 * Set up presets
+	 * (optional if not using presets)
+	 */
+	initPresets() {
 		let presets = []
 		this.setPresetDefinitions(presets)
 	}
 
-	actions(system) {
-		this.setActions({
+	/**
+	 * Set up actions
+	 */
+	updateActions() {
+		this.setActionDefinitions({
 			send: {
-				label: 'Send Command',
+				name: 'Send Command',
 				options: [
 					{
 						type: 'textwithvariables',
@@ -123,52 +136,39 @@ class instance extends instance_skel {
 						width: 6,
 					},
 				],
+				callback: async (action) => {
+					// Prepare the command
+					let cmd = await this.parseVariablesInString(action.options.id_send)
+					let start, end
+
+					if (this.config.type == 'engine') {
+						start = 'vizsend '
+						end = '\0' // or '\0\r\n'
+					} else {
+						start = ''
+						end = '\r\n'
+					}
+
+					// Split and send the commands
+					let cmds = cmd.split(';')
+
+					cmds.forEach((cmd) => {
+						let sendBuf = Buffer.from(start + cmd.trim() + end, 'latin1')
+
+						if (sendBuf != '') {
+							this.log('debug', 'Sending: ' + sendBuf + ' to ' + this.config.host)
+
+							if (this.socket !== undefined && this.socket.isConnected) {
+								this.socket.send(sendBuf)
+							} else {
+								this.log('debug', 'Socket not connected')
+							}
+						}
+					})
+				},
 			},
 		})
 	}
-
-	action(action) {
-		let cmd, start, end
-
-		switch (action.action) {
-			case 'send':
-				this.parseVariables(action.options.id_send, (value) => {
-					cmd = unescape(value)
-				})
-
-				if (this.config.type == 'engine') {
-					start = 'vizsend '
-					end = '\0' //or '\0\r\n'
-				} else {
-					start = ''
-					end = '\r\n'
-				}
-
-				break
-		}
-
-		/*
-		 * create a binary buffer pre-encoded 'latin1' (8bit no change bytes)
-		 * sending a string assumes 'utf8' encoding
-		 * which then escapes character values over 0x7F
-		 * and destroys the 'binary' content
-		 */
-
-		let cmds = cmd.split(';')
-
-		cmds.forEach((cmd) => {
-			let sendBuf = Buffer.from(start + cmd.trim() + end, 'latin1')
-
-			if (sendBuf != '') {
-				this.debug('sending ', sendBuf, 'to', this.config.host)
-
-				if (this.socket !== undefined && this.socket.connected) {
-					this.socket.send(sendBuf)
-				} else {
-					this.debug('Socket not connected :(')
-				}
-			}
-		})
-	}
 }
-exports = module.exports = instance
+
+export default VizrtTcpEngineTrioInstance
---
+++ webpack.config.js
@@ -0,0 +1,5 @@
+import { compileConfig } from '@companion-module/tools'
+
+export default compileConfig({
+	// Remove this if not using any special bundling
+})
